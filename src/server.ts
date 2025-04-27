cd33c79bd2f189da6fffcf63bc153d875bd1d97b
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-27 16:10:05 -0600

    funcionalidd crear proyecto, fix bug cors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
 import express from "express";
-import dontenv from "dotenv";
-
-import connectDB from "./config/db";
-import projectRoutes from "./routes/projectRoutes";
-
-dontenv.config()
-connectDB()
-
-
-const app = express()
-
-app.use(express.json())
-
-//routes
-app.use('/api/projects', projectRoutes)
-
-export default app
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { corsConfig } from "./config/cors";
+import  connectDB  from "./config/db";
+import projectRoutes from './routes/projectRoutes';
+ 
+dotenv.config();
+ 
+ 
+connectDB();
+ 
+const app = express();
+app.use(cors(corsConfig));
+app.use(express.json());
+ 
+// Routes 
+app.use('/api/projects', projectRoutes);
+ 
+export default app;
