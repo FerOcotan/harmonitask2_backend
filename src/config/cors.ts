32c3f342ed9dc153570d40f2b987a3d9787704d4
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-05-05 13:54:26 -0600

    update.cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,21 +1,33 @@
-import { CorsOptions } from "cors";
- 
+import { CorsOptions } from 'cors';
+
+
+
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
+
+  origin: (origin, callback) => {
+
     const whitelist = [process.env.FRONTEND_URL];
- 
- 
-    if(process.argv[2] === '--api') {
-      whitelist.push(undefined);
+
+
+
+    if (process.argv[2] === '--api') {
+
+      whitelist.push(undefined, null); // Permitir solicitudes sin origen (Postman, etc.)
+
     }
- 
+
+
+
     if (!origin || whitelist.includes(origin)) {
-      callback(null, true);
+
+      callback(null, true); // Permitir acceso
+
     } else {
-      
-      callback(new Error("Error de CORS"));
+
+      callback(new Error('Error de CORS: origen no permitido')); // Bloquear acceso
+
     }
+
   },
+
 };
- 
- 
